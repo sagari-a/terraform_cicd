fdf829198fa00bdd666076b8f8554adbe1ec3ed7
sagari-a <162323545+sagari-a@users.noreply.github.com>
2024-03-24 22:00:53 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-05295b6e6c790593e"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "jenkinsec2"
+    }
+}
